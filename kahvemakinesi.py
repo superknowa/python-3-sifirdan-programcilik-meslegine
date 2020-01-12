78a71bf9029ed1eb5937171ea08f85321145bf9a
superknowa <59803143+superknowa@users.noreply.github.com>
2020-01-13 01:23:19 +0300

    Add files via upload
    edit

--- kahvemakinesi.py
+++ kahvemakinesi.py
@@ -2,29 +2,32 @@ seker = input("Şeker kullanıyor musunuz?: ")
 
 secim = int(input("İçecek numaranızı girin: "))
 
-if secim == 1:
-    if seker == "evet":
-        print("Şeker Atılıyor")
-    elif seker == "hayır":
-        print("Şeker Atılmadı")
-    print("Filtre kahveniz hazırlanıyor.")  
-elif secim == 2:
-     if seker == "evet":
-        print("Şeker Atılıyor")
-     elif seker == "hayır":
-        print("Şeker Atılmadı") 
-     print("Espressonuz hazırlanıyor.")
-elif secim == 3:
-    if seker == "evet":
-        print("Şeker Atılıyor")
-    elif seker == "hayır":
-        print("Şeker Atılmadı")
-    print("Sıcak çikolatanız.")
-elif secim == 4:
-    if seker == "evet":
-        print("Şeker Atılıyor")
-    elif seker == "hayır":
-        print("Şeker Atılmadı")
-    print("Mochanız hazırlanıyor.")
+if seker:
+    if secim == 1:
+        if seker == "evet":
+            print("Şeker Atılıyor")
+        elif seker == "hayır":
+            print("Şeker Atılmadı")
+        print("Filtre kahveniz hazırlanıyor.")  
+    elif secim == 2:
+         if seker == "evet":
+            print("Şeker Atılıyor")
+         elif seker == "hayır":
+            print("Şeker Atılmadı") 
+         print("Espressonuz hazırlanıyor.")
+    elif secim == 3:
+        if seker == "evet":
+            print("Şeker Atılıyor")
+        elif seker == "hayır":
+            print("Şeker Atılmadı")
+        print("Sıcak çikolatanız.")
+    elif secim == 4:
+        if seker == "evet":
+            print("Şeker Atılıyor")
+        elif seker == "hayır":
+            print("Şeker Atılmadı")
+        print("Mochanız hazırlanıyor.")
+    else:
+        print("Menüde olmayan bir numara girdiniz.")
 else:
-    print("Menüde olmayan bir numara girdiniz.")
+    print("Şeker tercihi girilmemiş")
